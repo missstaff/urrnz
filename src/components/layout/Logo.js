819371354bb9fc515ac838231697e1933621267c
missstaff <shawnastaff@gmail.com>
2023-06-29 21:58:58 -0700

    basic footer component rendered on each page, logo scales nidely

---
+++ src/components/layout/Logo.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { useScreenSize } from "../../hooks/useScreenSize";
+
+
+const Logo = () => {
+
+    const screenSize = useScreenSize();
+    const [logoSize, setLogoSize] = useState(0);
+
+
+    useEffect(() => {
+        setSizes();
+    }, [screenSize]);
+
+
+    const setSizes = () => {
+        if (screenSize === "default") {
+            setLogoSize(1.4);
+        } else if (screenSize === "xs") {
+            setLogoSize(2.2);
+        } else if (screenSize === "sm") {
+            setLogoSize(2.2);
+        } else if (screenSize === "md") {
+            setLogoSize(3.4);
+        } else if (screenSize === "lg") {
+            setLogoSize(4.8);
+        } else if (screenSize === "xl") {
+            setLogoSize(5.5);
+        } else if (screenSize === "xxl") {
+            setLogoSize(6.5);
+        }
+    };
+
+
+    return (
+        <img
+            alt="Urrnz logo"
+            src={require("../../assets/logo.h200.beige.png")}
+            style={{
+                height: `${logoSize}rem`,
+                maxWidth: "25%"
+            }}
+        />
+    );
+};
+
+export default Logo;
--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
-
 import Footer from '../components/layout/Footer';
 import MainNavigation from '../components/layout/MainNavigation';
 
 
 const RootLayout = () => {
-    
     return (
         <div>
             <MainNavigation />
