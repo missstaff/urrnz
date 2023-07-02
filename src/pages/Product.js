96f167874d42bd7e653ed4f08162a3c81d643d07
missstaff <shawnastaff@gmail.com>
2023-07-01 22:23:32 -0700

    responsive product details page, time to clean up and refactor before moving to the cart and checkout process

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -17,34 +17,65 @@ const Product = () => {
 
 
     return (
-        <div style={{ margiLeft: 5, marginRight: 5 }}>
+        <div
+            className={classes.wrapper}>
             <div
                 className="headingContainer">
-                <Heading title="PRODUCT DETAILS" />
+                <Heading
+                    title="PRODUCT DETAILS" />
             </div>
             <NavLink
                 className={classes.link}
-                to={"/products"}
-            >{<span>&larr;</span>}Back</NavLink>
-            <div className="container" style={{ borderRadius: 7.5, backgroundColor: "rgba(255, 236, 209, 0.20)", marginBottom: `${9.6}rem`, padding: `${2.2}rem` }}>
-
-                <div className={`${classes.productContainer} ${classes.gridColumns}`}>
-
+                to={"/products"}>
+                {<span>&larr;</span>}Back
+            </NavLink>
+            <div
+                className={`container ${classes.container}`} >
+                <div
+                    className={`${classes.productContainer} ${classes.gridColumns}`}>
                     <img
                         className={classes.productImage}
                         src={product.images.lg}
                         alt={product.name}
                     />
                     <div>
-                        <div style={{marginBottom: `${2.2}rem`}}>
-                            <h3 style={{ fontSize: `${7.5}rem`, marginBottom: `${1}rem` }}>{product.name}</h3>
-                            <p style={{ fontSize: `${3.4}rem`, paddingBottom: `${1}rem`, marginBottom: `${1}rem` }}>{product.description}</p>
-                            <p style={{ fontSize: `${2.2}rem`, paddingBottom: `${0.1}rem` }}>Price: ${product.price}</p>
-                            <p style={{ fontSize: `${2.2}rem`, paddingBottom: `${0.1}rem` }}>Category: {product.category}</p>
-                            {product.color && <p style={{ fontSize: `${2.2}rem`, paddingBottom: `${0.1}rem` }}>Color: {product.color}</p>}
-                            {product.size && <p style={{ fontSize: `${2.2}rem`, paddingBottom: `${0.1}rem` }}>Size: {product.size}</p>}
+                        <div
+                            style={{ marginBottom: `${2.2}rem` }}>
+                            <h3
+                                className={classes.title}>{product.name}
+                            </h3>
+                            <p
+                                className={classes.description}>
+                                {product.description}
+                            </p>
+                            <p
+                                className={classes.detailText}>
+                                Price: ${product.price}
+                            </p>
+                            <p
+                                className={classes.detailText}>
+                                Category: {product.category}
+                            </p>
+                            {product.color
+                                &&
+                                (
+                                    <p
+                                        className={classes.detailText}>
+                                        Color: {product.color}
+                                    </p>
+                                )
+                            }
+                            {product.size
+                                &&
+                                (
+                                    <p
+                                        className={classes.detailText}>
+                                        Size: {product.size}
+                                    </p>
+                                )
+                            }
                         </div>
-                        <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", paddingBottom: `${2.2}rem`}}>
+                        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", paddingBottom: `${2.2}rem` }}>
                             <AddToCartButton />
                         </div>
                     </div>
