bae1e7200030b45f958a003a469ba1c688fa6963
missstaff <shawnastaff@gmail.com>
2023-07-13 18:46:09 -0700

    commit

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -9,72 +9,17 @@ import "../../general.css";
 
 const Hero = () => {
 
-    const screenSize = useScreenSize();
-    const [btnFontSize, setBtnFontSize] = useState(2.2);
-    const [fontSize, setFontSize] = useState(0);
-    const [verticalSpacing, setVerticalSpacing] = useState(0);
-
-
-    useEffect(() => {
-        const setSizes = () => {
-            if (screenSize === "default") {
-                setBtnFontSize(2.6);
-                setFontSize(3.4);
-                setVerticalSpacing(3.2);
-            } else if (screenSize === "xs") {
-                setBtnFontSize(3.0);
-                setFontSize(4.8);
-                setVerticalSpacing(2.8);
-            } else if (screenSize === "sm") {
-                setBtnFontSize(3.2);
-                setFontSize(4.8);
-                setVerticalSpacing(4.2);
-            } else if (screenSize === "md") {
-                setBtnFontSize(3.4);
-                setFontSize(5.6);
-                setVerticalSpacing(2.4);
-            } else if (screenSize === "lg") {
-                setBtnFontSize(4.4);
-                setFontSize(10.8);
-                setVerticalSpacing(3.2);
-            } else if (screenSize === "xl") {
-                setBtnFontSize(5.2);
-                setFontSize(12.6);
-                setVerticalSpacing(5.6);
-            } else if (screenSize === "xxl") {
-                setBtnFontSize(6.4);
-                setFontSize(15);
-                setVerticalSpacing(6.8);
-            }
-        };
-        setSizes();
-    }, [screenSize]);
-
-
     return (
-        <div
-            className={classes.hero}>
-            <div
-                className={classes.content}>
-                <h1
-                    className={classes.title}
-                    style={{
-                        fontSize: `${fontSize}rem`,
-                        marginBottom: `${verticalSpacing}rem`,
-                    }}>
-                    <p
-                        style={{ textTransform: "uppercase" }}>
-                        URRNZ CUSTOM KEEPSAKES
-                    </p>
+        <div className={classes.hero}>
+            <div className={classes.content}>
+                <h1 className={classes.title}>
+                    URRNZ CUSTOM KEEPSAKES
                 </h1>
-                <div
-                    style={{ marginTop: `${verticalSpacing}rem` }}>
-                    <StoreButton
-                        to="/products/all"
-                        title="SHOP NOW"
-                        style={{ fontSize: `${btnFontSize}rem`, }} 
-                    />
-                </div>
+                <StoreButton
+                    to="/products/all"
+                    title="SHOP NOW"
+                    style={{ fontSize: `${4.8}rem`, }}
+                />
             </div>
         </div>
     );
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,32 @@
+import { useRouteError, NavLink } from 'react-router-dom';
+import PageContent from '../components/PageContent';
+import classes from './Error.module.css';
+
 const Error = () => {
-    // needs a component to be dynamic
+
+    const error = useRouteError();
+
+    let title = "An error occured.";
+    let message = "Sorry, something went wrong.";
+
+    if (error.status === 404) {
+        title = "Page not found!";
+        message = "Sorry, the page you were looking for does not exist.";
+    }
+
+    if (error.status === 500) {
+        title = "Server error";
+        message = "Sorry, the server is not responding";
+    }
+
+
     return (
-        <div>
-            <h1>Error</h1>
-        </div>
+        <main style={{ display: "flex", flexDirection: "column", justifyContent: "center", height: `${100}vh`, width: "100%" }}>
+            <PageContent titleClassName={classes.title} title={title}>
+                <p className={classes.message}>{message}</p>
+                <NavLink className={classes.link} to="/">{<span>&larr;</span>}Return to Urrnz</NavLink>
+            </PageContent>
+        </main>
     );
 };
 
