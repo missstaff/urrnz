0a411ec75640d3e1d2187da05ce8cae06ac0142c
missstaff <shawnastaff@gmail.com>
2023-07-14 00:31:25 -0700

    sticky nav bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FaGripLines } from "react-icons/fa";
 import { NavLink, useLocation } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link";
@@ -8,154 +8,97 @@ import { useScreenSize } from "../../hooks/useScreenSize";
 
 import classes from "./MainNavigation.module.css";
 
-
 function MainNavigation() {
+  const location = useLocation();
 
-    const screenSize = useScreenSize();
-    const location = useLocation();
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [mobileNavBtnSize, setMobileNavBtnSize] = useState(0);
 
-    const [isMenuVisible, setIsMenuVisible] = useState(false);
-    const [mobileNavBtnSize, setMobileNavBtnSize] = useState(0);
-    const [isSticky, setIsSticky] = useState(false);
+  const curLocation = location.pathname;
+  const pathHash = location.hash;
 
-    const curLocation = location.pathname;
-    const pathHash = location.hash;
+  const handleMobileNavClick = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
 
+  const handleNavLinkClick = () => {
+    setIsMenuVisible(false);
+  };
 
-    useEffect(() => {
-        const setSizes = () => {
-            if (screenSize === "default") {
-                setMobileNavBtnSize(2.2);
-            } else if (screenSize === "xs") {
-                setMobileNavBtnSize(3.4);
-            } else if (screenSize === "sm") {
-                setMobileNavBtnSize(4.8);
-            } else if (screenSize === "md") {
-                setMobileNavBtnSize(5.5);
-            }
-        };
-        setSizes();
-    }, [screenSize,]);
+  if (curLocation === "/" && pathHash !== "#about" && pathHash !== "#faqs") {
+    window.scrollTo(0, 0);
+  }
 
+  return (
+    <header className={`${classes.header} ${isMenuVisible ? classes.navOpen : ""}`}>
+      <LogoHeader />
+      <nav className={`${classes.mainNav} ${isMenuVisible ? classes.mobileNav : ""}`}>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              onClick={handleNavLinkClick}
+              isActive={(match, location) => match && curLocation === "/"}
+              end
+            >
+              <p>Home</p>
+            </NavLink>
+          </li>
+          <li>
+            <Link
+              to="/#about"
+              onClick={handleNavLinkClick}
+              isActive={() => curLocation === "/" && pathHash === "#about"}
+            >
+              <p>About</p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/#faqs"
+              onClick={handleNavLinkClick}
+              isActive={() => curLocation === "/" && pathHash === "#faqs"}
+            >
+              <p>FAQ</p>
+            </Link>
+          </li>
+          <li>
+            <NavLink to="/genres" onClick={handleNavLinkClick} isActive={() => curLocation === "/genres"}>
+              <p>Genres</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products/all"
+              onClick={handleNavLinkClick}
+              isActive={() => curLocation.startsWith("/products")}
+            >
+              <p>Gallery</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" onClick={handleNavLinkClick} isActive={() => curLocation === "/cart"}>
+              <p>Cart</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={handleNavLinkClick} isActive={() => curLocation === "/contact"}>
+              <p>Contact</p>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
 
-
-    useEffect(() => {
-        const sectionAboutEl = document.querySelector("#home");
-        const observer = new IntersectionObserver((entries) => {
-            const ent = entries[0];
-            if (!ent.isIntersecting) {
-                setIsSticky(true);
-            }
-            if (ent.isIntersecting) {
-                setIsSticky(false);
-            }
-        }, { root: null, threshold: 0 })
-        observer.observe(sectionAboutEl);
-    }, []);
-
-     // Scroll to top when returning to home
-    useEffect(() => {
-        if (curLocation === "/" && pathHash !== "#about" && pathHash !== "#faqs") {
-            window.scrollTo(0, 0);
-          }
-    }, [curLocation, pathHash]);
-
-
-
-    const handleMobileNavClick = () => {
-        setIsMenuVisible(!isMenuVisible);
-    };
-
-
-
-    return (
-        <header
-            className={`${isSticky ? classes.sticky : ""} ${classes.header} ${isMenuVisible ? `${classes.navOpen}` : ""} `}>
-            <LogoHeader />
-            <nav
-                className={`${classes.mainNav} ${isMenuVisible ? `${classes.mobileNav}` : ""}`}>
-                <ul
-                    className={classes.list}>
-                    <li>
-                        <NavLink
-                            to="/"
-                            onClick={isMenuVisible && handleMobileNavClick}
-                            className={({ isActive }) => isActive && curLocation === "/" ? classes.active : undefined}
-                            end>
-                            <p>Home</p>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <Link
-                            to="/#about"
-                            onClick={() => isMenuVisible && handleMobileNavClick()}
-                            className={curLocation === "/" && pathHash === "#about" ? classes.active : undefined}>
-                            <p>About</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            to="/#faqs"
-                            onClick={() => isMenuVisible && handleMobileNavClick()}
-                            className={curLocation === "/" && pathHash === "#faqs" ? classes.active : undefined}>
-                            <p>FAQ</p>
-                        </Link>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/genres"
-                            onClick={isMenuVisible && handleMobileNavClick}
-                            className={({ isActive }) => isActive ? classes.active : undefined}>
-                            <p>Genres</p>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/products/all"
-                            onClick={isMenuVisible && handleMobileNavClick}
-                            className={({ isActive }) => isActive ? classes.active : undefined}>
-                            <p>Gallery</p>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/cart"
-                            onClick={isMenuVisible && handleMobileNavClick}
-                            className={({ isActive }) => isActive ? classes.active : undefined}>
-                            <p>Cart</p>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/contact"
-                            onClick={isMenuVisible && handleMobileNavClick}
-                            className={({ isActive }) => isActive ? classes.active : undefined}>
-                            <p>Contact</p>
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-
-
-            <button className={classes.mobileNav} onClick={handleMobileNavClick}>
-                <NavLink
-                    className={classes.iconMobileNav}
-                    name="menu-outline">
-                    <FaGripLines
-                        size={`${mobileNavBtnSize}rem`}
-                        color="rgba(255, 71, 0, 1)" />
-                </NavLink>
-                <NavLink
-                    className={classes.iconMobileNav}
-                    name="close-outline">
-                    <FaGripLines
-                        size={`${mobileNavBtnSize}rem`}
-                        color="rgba(255, 71, 0, 1)"
-                    />
-                </NavLink>
-            </button>
-        </header>
-    );
-};
+      <button className={classes.mobileNav} onClick={handleMobileNavClick}>
+        <NavLink className={classes.iconMobileNav} name="menu-outline">
+          <FaGripLines size={`${mobileNavBtnSize}rem`} color="rgba(255, 71, 0, 1)" />
+        </NavLink>
+        <NavLink className={classes.iconMobileNav} name="close-outline">
+          <FaGripLines size={`${mobileNavBtnSize}rem`} color="rgba(255, 71, 0, 1)" />
+        </NavLink>
+      </button>
+    </header>
+  );
+}
 
 export default MainNavigation;
