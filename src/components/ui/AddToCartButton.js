ec581050aa320bc18a5ef1c17aa55fa5d1afea84
missstaff <shawnastaff@gmail.com>
2023-11-12 09:23:15 -0800

    size adjustments/css updates to the product view

--- src/components/ui/AddToCartButton.js
+++ src/components/ui/AddToCartButton.js
@@ -5,9 +5,10 @@ import classes from "./AddToCartButton.module.css";
 const AddToCartButton = (props) => {
     return (
         <NavLink
-            className={classes.button}
+            className={[`${classes.button}`]}
             exact="true"
             onClick={props.onClick}
+            style={props.style}
             to="/cart">
             ADD TO CART
         </NavLink>
